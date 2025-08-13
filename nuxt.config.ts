ab6b3c26bdf060d071e3256e6a21eb10571600ce
Alexander Sheigov <alexander.sheigov@gmail.com>
2025-08-13 12:26:30 +0600

    Инициализация проекта

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  
+  // App configuration
+  app: {
+    head: {
+      title: 'Поиск квартир',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: 'Приложение для поиска квартир с фильтрацией' }
+      ]
+    }
+  },
+})
